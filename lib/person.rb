40aa9b397dd2deb5cbc84b056715db1566582450
Rafael Lopez <lpz.raf@gmail.com>
2018-07-08 22:51:21 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,9 +1,9 @@
 class Person 
-  def initialize
-  @name = "Beyonce"
+  def initialize(name)
+  @name = name
   end
   
-  def name= (name)
+  def name=(name)
   @name = name
   end
   
@@ -12,34 +12,5 @@ class Person
   end
 end
 
-beyonce = Person.new
-beyonce.name = "Beyonce"
-  puts beyonce.name
+beyonce = Person.new("Beyonce")
 
-# ===================
-# class Dog 
-#   def name= (fido_name)
-#     @name = fido_name
-#   end
-  
-#   def name
-#     @name
-#   end
-  
-#   def breed= (breed)
-#     @breed = breed
-#   end
-  
-#   def breed
-#     @breed
-#   end
-# end
-
-
-# fido = Dog.new
-# fido.name = "Fido"
-#   puts fido.name
-
-# snoopy = Dog.new
-# snoopy.breed = "Beagle"
-#   puts snoopy.breed
